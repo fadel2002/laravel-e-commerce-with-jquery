1089a7c1d0912815a0ec556acbee00e710aa362c
fadelpm2002 <fadelpm2002@gmail.com>
2022-11-02 22:38:18 +0700

    adding layouts

---
+++ resources/views/auth/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+<div>
+    auth
+</div>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Ogani | Template</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/elegant-icons.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/nice-select.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/jquery-ui.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/slicknav.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
+    @stack('style')
+</head>
+
+<body>
+    <!--::header part start::-->
+    @include('layouts.partials.header')
+    <!-- Header part end-->
+
+    @yield('content')
+
+    <!--::footer_part start::-->
+    @include('layouts.partials.footer')
+    <!--::footer_part end::-->
+
+    <!-- Js Plugins -->
+    <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
+    <script src="{{asset('js/jquery-ui.min.js')}}"></script>
+    <script src="{{asset('js/jquery.slicknav.js')}}"></script>
+    <script src="{{asset('js/mixitup.min.js')}}"></script>
+    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('js/main.js')}}"></script>
+    @stack('script')
+    @include('sweetalert::alert')
+
+</body>
+
+</html>
