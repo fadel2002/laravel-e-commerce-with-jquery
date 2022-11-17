51cfd8a077144463c9e79beb39dbee92c534b91d
fadelpm2002 <fadelpm2002@gmail.com>
2022-11-17 22:05:49 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,61 +2,25 @@
 
 @section('content')
 <div>
-    <!-- Hero Section Begin -->
-    <section class="hero">
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3">
-                    <div class="hero__categories">
-                        <div class="hero__categories__all">
-                            <i class="fa fa-bars"></i>
-                            <span>All departments</span>
-                        </div>
-                        <ul>
-                            @foreach($data['kategori'] as $kategori)
-                            <li><a href="#">{{ $kategori }}</a></li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-9">
-                    <div class="hero__search">
-                        <div class="hero__search__form">
-                            <form action="#">
-                                <div class="hero__search__categories">
-                                    All Categories
-                                    <span class="arrow_carrot-down"></span>
-                                </div>
-                                <input type="text" placeholder="What do yo u need?">
-                                <button type="submit" class="site-btn">SEARCH</button>
-                            </form>
-                        </div>
-                        <div class="hero__search__phone">
-                            <div class="hero__search__phone__icon">
-                                <i class="fa fa-phone"></i>
-                            </div>
-                            <div class="hero__search__phone__text">
-                                <h5>+65 11.188.888</h5>
-                                <span>support 24/7 time</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="hero__item set-bg" data-setbg="img/hero/banner.jpg">
-                        <div class="hero__text">
-                            <span>FRUIT FRESH</span>
-                            <h2>Vegetable <br />100% Organic</h2>
-                            <p>Free Pickup and Delivery Available</p>
-                            <a href="#" class="primary-btn">SHOP NOW</a>
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2>My Warung</h2>
+                        <div class="breadcrumb__option">
+                            <span>Free Pickup and Delivery Available</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <!-- Hero Section End -->
+    <!-- Breadcrumb Section End -->
 
     <!-- Categories Section Begin -->
-    <section class="categories">
+    <section class="categories mt-5">
         <div class="container">
             <div class="row">
                 <div class="categories__slider owl-carousel">
@@ -74,7 +38,7 @@
     <!-- Categories Section End -->
 
     <!-- Featured Section Begin -->
-    <section class="featured spad">
+    <section class="featured">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
